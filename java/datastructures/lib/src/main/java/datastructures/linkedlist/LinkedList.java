0ba78469ad4a5f90165dc1b41fa340afd8a93ecc
wilsonjhub <jasonstimphil@gmail.com>
2022-06-04 00:19:37 -0400

    completed code challenge

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -30,14 +30,41 @@ public class LinkedList {
     }
     return false;
   }
-  @Override
-  public String toString(){
-    String temp = "";
-    Node current = head;
-    while(current != null){
-      temp = temp + "[" + current.value + "] -> ";
-      current = current.next;
-    }
-    return temp + "null";
-  }
+//  @Override
+//  public String toString(){
+//    String temp = "";
+//    Node current = head;
+//    while(current != null){
+//      temp = temp + "[" + current.value + "] -> ";
+//      current = current.next;
+//    }
+//    return temp + "null";
+//  }
+//
+//  // --- Lab 8 ZipList ---
+//  public boolean zipList (LinkedList<T> otherList) {
+//    try {
+//      Node<T> tempAlpha = this.head;
+//      Node<T> tempBravo = otherList.head;
+//      Node<T> tempCharlie = this.head.next;
+//
+//      while (tempAlpha.next != null) {
+//        tempCharlie = tempAlpha.next;
+//
+//        tempAlpha.next = tempBravo;
+//        otherList.head = otherList.head.next;
+//        tempBravo.next = tempCharlie;
+//        tempBravo = otherList.head;
+//        tempAlpha = tempCharlie;
+//
+//      }
+//      tempAlpha.next =tempBravo;
+//    } catch ( Exception ex) {
+//      System.out.println("something went wrong: " + ex.getMessage());
+//      return false;
+//
+//    }
+//    return true;
+//  }
+
 }
---
+++ java/datastructures/lib/src/main/java/datastructures/trees/BinaryTree.java
@@ -0,0 +1,48 @@
+package datastructures.trees;
+import java.util.ArrayList;
+
+public class BinaryTree<T> {
+  public ArrayList<Integer> tNodeArrayList;
+  public Node<Integer> tNode;
+
+  public BinaryTree(){
+    tNode = new Node<>();
+    tNodeArrayList = new ArrayList<>();
+  }
+
+  //In preOrder traversal, you visit the root Node first, then the left subTree, and then push Data to array.
+  // Data->Left->Right
+  public void traversePreOrder(Node<Integer> tNode) {
+    if (tNode != null) {
+      tNodeArrayList.add(tNode.value);
+      traversePreOrder(tNode.left);
+      traversePreOrder(tNode.right);
+    }
+  }
+
+    // In inOrder traversal, you visit the left subTree, then the root Node, and then push Data to array.
+    // Left->Data->Right
+    public void traverseInOrder(Node<Integer> tNode){
+      if(tNode != null){
+        traverseInOrder(tNode.left);
+        tNodeArrayList.add(tNode.value);
+        traverseInOrder(tNode.right);
+
+      }
+    }
+
+    // In postOrder traversal, you visit the left subTree, then the right subTree, and then push Data to array.
+    // Left->Right->Data
+  public void traversePostOrder(Node<Integer> tNode){
+    if(tNode != null){
+      traverseInOrder(tNode.left);
+      traverseInOrder(tNode.right);
+      tNodeArrayList.add(tNode.value);
+
+    }
+  }
+
+  }
+
+
+
