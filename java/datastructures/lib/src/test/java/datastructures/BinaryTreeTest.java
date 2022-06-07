518b447eab7252b7d07dea8657ff986ea1566ac7
wilsonjhub <jasonstimphil@gmail.com>
2022-06-07 17:19:02 -0400

    added test for maxValOfTree method

---
+++ java/datastructures/lib/src/test/java/datastructures/BinaryTreeTest.java
@@ -0,0 +1,12 @@
+package datastructures;
+import datastructures.trees.BinaryTree;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinaryTreeTest {
+  @Test
+  void testingMaxTreeValue() {
+    BinaryTree sut = new BinaryTree();
+    assertTrue(sut != null);
+  }
+}
