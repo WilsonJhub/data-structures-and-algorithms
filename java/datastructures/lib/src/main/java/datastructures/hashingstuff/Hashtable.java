0144842493befa08e4070203f0e95c8085006a43
wilsonjhub <jasonstimphil@gmail.com>
2022-06-21 21:02:47 -0400

    need to add set function for HashTable

---
+++ java/datastructures/lib/src/main/java/datastructures/hashingstuff/Hashtable.java
@@ -0,0 +1,19 @@
+package datastructures.hashingstuff;
+
+public class Hashtable {
+
+  int key;
+  String value;
+
+  Hashtable<Integer, String> table = new Hashtable()<>(10);
+
+//  public Hashtable(int key, String value) {
+//    this.key = key;
+//    this.value = value;
+//
+//    return;
+//  }
+
+
+
+}
